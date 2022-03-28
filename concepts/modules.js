e7b0aea2536fd3ae73217844eaab37cd73638466
Abdulaziz <dev.abdulaziz@outlook.com>
2022-03-28 11:21:12 +0300

    MVC Blog

--- concepts/modules.js
+++
@@ -1,8 +0,0 @@
-const {people , ages} = require('./people');
-
-console.log('module file ',people ,ages);
-
-const os = require('os')
-console.log(os);
-console.log('br');
-console.log(os.platformm ,os.homedir);
--- concepts/people.js
+++
@@ -1,8 +0,0 @@
-const people=['norah','az','melfy','omar'];
-const ages = [ 44, 24,60,30];
-// console.log('people file ',people);
-
-// module.exports = people;
-module.exports = {
-people , ages
-}
